cf9d8498f8cc2e412ece7358e80344d71b151eae
Superman <supermanchc@gmail.com>
2018-05-20 23:23:08 +0800

    add updateElement function

--- dist/index.js
+++ dist/index.js
@@ -1,53 +1,10 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 /** @jsx h */
-var items = ['github', 'facebook', 'google'];
 
-var vdom = render(h(
-  'div',
-  null,
-  h(
-    'p',
-    null,
-    'his is an HTML page with a single babel-transpiled JS file and no dependencies. It is rendering DOM via JSX without any frameworks.'
-  ),
-  h(
-    'p',
-    null,
-    'Simple JSX DOM Render'
-  ),
-  h(
-    'ul',
-    null,
-    items.map(function (item) {
-      return h(
-        'li',
-        null,
-        item
-      );
-    })
-  )
-));
-
-window.onload = function () {
-  document.body.appendChild(vdom);
-};
-
-function render(vnode) {
-  // 如果 vnode 是 string，直接转换为 TextNode 即可
-  if (typeof vnode === 'string') return document.createTextNode(vnode);
-  var n = document.createElement(vnode.nodeName);
-  var a = vnode.attributes || {};
-  Object.keys(a).forEach(function (k) {
-    return n.setAttributes(k, a[k]);
-  });
-  vnode.children.forEach(function (c) {
-    return n.appendChild(render(c));
-  });
-  return n;
-}
-
-function h(nodeName, attributes) {
+function h(type, props) {
   var _ref;
 
   for (var _len = arguments.length, args = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
@@ -55,5 +12,107 @@ function h(nodeName, attributes) {
   }
 
   var children = args.length ? (_ref = []).concat.apply(_ref, args) : null;
-  return { nodeName: nodeName, attributes: attributes, children: children };
+  return { type: type, props: props, children: children };
 }
+
+function createElement(vnode) {
+  // 如果 vnode 是 string，直接转换为 TextNode 即可
+  if (typeof vnode === 'string') {
+    return document.createTextNode(vnode);
+  }
+  var n = document.createElement(vnode.type);
+  var a = vnode.props || {};
+  Object.keys(a).forEach(function (k) {
+    return n.setAttributes(k, a[k]);
+  });
+  vnode.children.forEach(function (c) {
+    return n.appendChild(createElement(c));
+  });
+  return n;
+}
+
+/**
+ * 更新 node
+ * @param {*}
+ */
+function updateElement($parent, newNode, oldNode) {
+  var index = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
+
+  // 如果 oldNode 不存在，则使用 appendChild()
+  if (!oldNode) {
+    $parent.appendChild(createElement(newNode));
+  }
+  // 如果 newNode 不存在，则需要将 oldNode remove
+  else if (!newNode) {
+      $parent.removeChild($parent.childNodes[index]);
+    }
+    // 节点发生变化,则replace node
+    else if (changed(newNode, oldNode)) {
+        $parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
+      }
+      // diff children, 递归调用 updateElement 判断 children 是否变化
+      else if (newNode.type) {
+          var newLength = newNode.children.length;
+          var oldLength = oldNode.children.length;
+          for (var i = 0; i < newLength || i < oldLength; i++) {
+            console.log($parent);
+            updateElement($parent.childNodes[index], newNode.children[i], oldNode.children[i], i);
+          }
+        }
+}
+
+/**
+ * 判断节点是否改变
+ * @param {*} node1
+ * @param {*} node2
+ */
+function changed(node1, node2) {
+  return (typeof node1 === 'undefined' ? 'undefined' : _typeof(node1)) !== (typeof node2 === 'undefined' ? 'undefined' : _typeof(node2)) || typeof node1 === 'string' && node1 !== node2 || node1.type !== node2.type;
+}
+
+var oldItems = ['github', 'facebook', 'google'];
+var newItems = ['Twitter', 'Gmail', 'Paypal'];
+
+var oldVdom = h(
+  'ul',
+  null,
+  oldItems.map(function (item) {
+    return h(
+      'li',
+      null,
+      item
+    );
+  })
+);
+
+var newVdom =
+/*   <div>
+  <p>
+    his is an HTML page with a single babel-transpiled JS file and no
+    dependencies. It is rendering DOM via JSX without any frameworks.
+  </p>
+  <p>Simple JSX DOM Render</p> */
+h(
+  'ul',
+  null,
+  newItems.map(function (item) {
+    return h(
+      'li',
+      null,
+      item
+    );
+  })
+)
+// </div>
+;
+
+window.onload = function () {
+  var app = document.getElementById('app');
+  console.log(app);
+  var reload = document.getElementById('reload');
+
+  updateElement(app, oldVdom);
+  reload.addEventListener('click', function () {
+    updateElement(app, newVdom, oldVdom);
+  });
+};
--- index.js
+++ index.js
@@ -1,32 +1,89 @@
 /** @jsx h */
-const items = ['github', 'facebook', 'google']
 
-const vdom = render(
-  <div>
+function h(type, props, ...args) {
+  let children = args.length ? [].concat(...args) : null
+  return { type, props, children }
+}
+
+function createElement(vnode) {
+  // 如果 vnode 是 string，直接转换为 TextNode 即可
+  if (typeof vnode === 'string') {
+    return document.createTextNode(vnode)
+  }
+  let n = document.createElement(vnode.type)
+  let a = vnode.props || {}
+  Object.keys(a).forEach(k => n.setAttributes(k, a[k]))
+  vnode.children.forEach(c => n.appendChild(createElement(c)))
+  return n
+}
+
+/**
+ * 更新 node
+ * @param {*}
+ */
+function updateElement($parent, newNode, oldNode, index = 0) {
+  // 如果 oldNode 不存在，则使用 appendChild()
+  if (!oldNode) {
+    $parent.appendChild(createElement(newNode))
+  }
+  // 如果 newNode 不存在，则需要将 oldNode remove
+  else if (!newNode) {
+    $parent.removeChild($parent.childNodes[index])
+  }
+  // 节点发生变化,则replace node
+  else if (changed(newNode, oldNode)) {
+    $parent.replaceChild(createElement(newNode), $parent.childNodes[index])
+  }
+  // diff children, 递归调用 updateElement 判断 children 是否变化
+  else if (newNode.type) {
+    const newLength = newNode.children.length
+    const oldLength = oldNode.children.length
+    for (let i = 0; i < newLength || i < oldLength; i++) {
+      updateElement(
+        $parent.childNodes[index],
+        newNode.children[i],
+        oldNode.children[i],
+        i,
+      )
+    }
+  }
+}
+
+/**
+ * 判断节点是否改变
+ * @param {*} node1
+ * @param {*} node2
+ */
+function changed(node1, node2) {
+  return (
+    typeof node1 !== typeof node2 ||
+    (typeof node1 === 'string' && node1 !== node2) ||
+    node1.type !== node2.type
+  )
+}
+
+const oldItems = ['github', 'facebook', 'google']
+const newItems = ['Twitter', 'Gmail', 'Paypal']
+
+const oldVdom = <ul>{oldItems.map(item => <li>{item}</li>)}</ul>
+
+const newVdom = (
+  /*   <div>
     <p>
       his is an HTML page with a single babel-transpiled JS file and no
       dependencies. It is rendering DOM via JSX without any frameworks.
     </p>
-    <p>Simple JSX DOM Render</p>
-    <ul>{items.map(item => <li>{item}</li>)}</ul>
-  </div>,
+    <p>Simple JSX DOM Render</p> */
+  <ul>{newItems.map(item => <li>{item}</li>)}</ul>
+  // </div>
 )
 
 window.onload = function() {
-  document.body.appendChild(vdom)
-}
+  const app = document.getElementById('app')
+  const reload = document.getElementById('reload')
 
-function render(vnode) {
-  // 如果 vnode 是 string，直接转换为 TextNode 即可
-  if (typeof vnode === 'string') return document.createTextNode(vnode)
-  let n = document.createElement(vnode.nodeName)
-  let a = vnode.attributes || {}
-  Object.keys(a).forEach(k => n.setAttributes(k, a[k]))
-  vnode.children.forEach(c => n.appendChild(render(c)))
-  return n
-}
-
-function h(nodeName, attributes, ...args) {
-  let children = args.length ? [].concat(...args) : null
-  return { nodeName, attributes, children }
+  updateElement(app, oldVdom)
+  reload.addEventListener('click', () => {
+    updateElement(app, newVdom, oldVdom)
+  })
 }
